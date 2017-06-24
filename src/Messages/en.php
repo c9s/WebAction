449b3e12b32f1cfc4fc5a765ce232a4e982118ca
c9s <yoanlin93@gmail.com>
2017-06-24 22:04:37 +0800

    improve validation error message

--- src/Messages/en.php
+++ src/Messages/en.php
@@ -5,7 +5,7 @@
 return [
     'file.required'                         => 'File field %1 is required.',
     'param.required'                        => 'Field %1 is required.',
-    'validation.error'                      => 'Please check your input.',
+    'validation.error'                      => 'Validation failed. Please check your input.',
     'csrf.token_expired'                    => 'CSRF token is expired, please refresh your page.',
     'csrf.token_mismatch'                   => 'CSRF Token mismatched.',
     'csrf.token_invalid'                    => 'CSRF token invalid.',
